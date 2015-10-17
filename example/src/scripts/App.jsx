84c0f3b27fdc35fb3503292046bcb943c93d32ca
Igor Prado <igorprado@gmail.com>
2015-10-16 23:55:05 -0300

    Improved example page

--- example/src/scripts/App.jsx
+++ example/src/scripts/App.jsx
@@ -119,10 +119,13 @@ NotificationSystemExample = React.createClass({
           <div className="content">
             <h1 className="title">React Notification System</h1>
             <h2 className="subtitle">A complete and totally customizable component for notifications in React.</h2>
-            <h3 className="versions">(For React 0.14x and 0.13.x)</h3>
+            <h3 className="versions">(For React 0.14 and 0.13)</h3>
 
             <div className="btn-show-magic-holder">
-              <button className="btn btn-outline btn-show-magic" onClick={ this._showTheMagic }>Show me what it can do!</button>
+              <button className="btn btn-outline btn-show-magic" onClick={ this._showTheMagic }>
+                Show me what it can do!
+              </button>
+              <span className="width-warning">Better experience in larger screens</span>
               <small className="more-magic">Click twice for more awesomeness!</small>
             </div>
             <div className="github-buttons">
